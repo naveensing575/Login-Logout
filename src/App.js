7c0ed0c1d4438ec27781911d7e6abcf037f6844a
naveen.singh <naveen.singh@itelasoft.com>
2023-10-12 16:50:22 +0530

    remove localStorage when logout

--- src/App.js
+++ src/App.js
@@ -14,14 +14,12 @@ function App() {
     }
   }, []);
   const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
     setIsLoggedIn(true);
     localStorage.setItem("isLoggedIn", "1");
   };
 
   const logoutHandler = () => {
-    localStorage.setItem("isLoggedIn", "0");
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
